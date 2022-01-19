148232a9bfaa7abe397ce308ade0e37c3e0c7d8e
dxd-lab <89575789+dxd-lab@users.noreply.github.com>
2022-01-19 23:03:49 +0900

    Initial Commit

---
+++ myThingun/myThingunApp.swift
@@ -0,0 +1,17 @@
+//
+//  myThingunApp.swift
+//  myThingun
+//
+//  Created by Taewan Kim on 2022/01/19.
+//
+
+import SwiftUI
+
+@main
+struct myThingunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
